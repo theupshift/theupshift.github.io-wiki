b0341308811b2eab3faec86d4fbce647c85cb13e
Josh Avanier <jkvnr@users.noreply.github.com>
2016-06-28 18:05:50 +0700

    deletion

--- a.js
+++
@@ -1,1 +0,0 @@
-(function(a,e,f,g,b,c,d){a.GoogleAnalyticsObject=b;a[b]=a[b]||function(){(a[b].q=a[b].q||[]).push(arguments)};a[b].l=1*new Date;c=e.createElement(f);d=e.getElementsByTagName(f)[0];c.async=1;c.src=g;d.parentNode.insertBefore(c,d)})(window,document,"script","https://www.google-analytics.com/analytics.js","ga");ga("create","UA-79171090-1","auto");ga("send","pageview");
