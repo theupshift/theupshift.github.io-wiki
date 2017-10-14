0106949661929d216b8c4ccd9803fb5beb9f5886
Josh Avanier <joshavanier@gmail.com>
2017-10-15 04:32:36 +0700

    Fix

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,3 @@
-d("date", Aequirys.dis(Aequirys.con()))
+d("date", Aequirys.display(Aequirys.convert()))
 d("mortem", Mors.pro(new Date(1997, 2, 17)).toFixed(2) + "%")
 function d(i, c) {document.getElementById(i).innerHTML = c}
