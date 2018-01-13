34c52830bd2bea9c045299a31d64067155d2b3d1
Josh Avanier <joshavanier@gmail.com>
2018-01-13 11:39:43 +0700

    Remove Mors

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,1 @@
-d("date", Aequirys.display(Aequirys.convert()))
-d("mortem", Mors.pro(new Date(1997, 2, 17)).toFixed(2) + "%")
-function d(i, c) {document.getElementById(i).innerHTML = c}
+document.getElementById("date").innerHTML = Aequirys.display(Aequirys.convert())
