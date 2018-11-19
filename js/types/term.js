05a13551c43f53c79cfdb3b75c574d108522ce6a
Josh Avanier <joshavanier@gmail.com>
2018-11-19 11:08:22 +0700

    Switch BREF to ?

--- js/types/term.js
+++ js/types/term.js
@@ -1,7 +1,7 @@
 function Term (name, dict = {}, unde = 'Home', type = 'none') {
   Object.assign(this, {
     name, type, unde,
-    bref: dict.BREF ? toMarkup(dict.BREF) : '',
+    bref: dict['?'] ? toMarkup(dict['?']) : '',
     long: new Runic(dict['$']).html()
   });
 }
