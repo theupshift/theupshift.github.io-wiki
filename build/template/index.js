b3b7a4f5b4fe83f1e24cf46b44be564b09b19bb1
Josh Avanier <joshavanier@gmail.com>
2018-12-16 21:23:41 +0700

    Redesign index

--- build/template/index.js
+++ build/template/index.js
@@ -29,15 +29,15 @@ module.exports = function Index ({term, unde, type, line}) {
       const child = children[id];
       const {term, line} = child;
       const link = `<a href="./${term.toUrl()}.html">${term.toCapitalCase()}</a>`
-      html += `<dt>${link}</dt><dd>${child.line['?']}</dd>`;
+      html += `<tr><td>${link}</td><td>${child.line['?']}</td></tr>`;
     }
 
-    return children.length > 0 ? `<dl>${html}</dl>` : '';
+    return children.length > 0 ? `<div id="i"><table><tbody>${html}</tbody></table></div>` : '';
   }
 
   this.render = () => {
     const {id, parent} = this;
     const parentURL = parent === 'Home' ? 'index' : parent.toUrl();
-    return `<!doctype html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><meta name="author" content="Josh Avanier"><title>${id.toCapitalCase()}</title><link rel="stylesheet" href="./s.css"/></head><body><div id="v"><p id="u"><a href="./${parentURL}.html">${parent.toCapitalCase()}</a></p><input id="s" value="${id.toCapitalCase()}" spellcheck="false"><main id="c">${_core(id, parent)}${makeIndex(id)}</main><footer id="f"><a href="http://webring.xxiivv.com/#random" target="_blank"><img id="w" src="./img/rotonde.svg"></a><p><a href="./josh.html">Josh Avanier</a> © Éternité</footer></div><script src="./search.js"></script></body></html>`;
+    return `<!doctype html><html lang="en"><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><meta name="author" content="Josh Avanier"><title>${id.toCapitalCase()}</title><link rel="stylesheet" href="./s.css"/></head><body><div id="v"><p id="u"><a href="./${parentURL}.html">${parent.toCapitalCase()}</a></p><input id="s" value="${id.toCapitalCase()}" spellcheck="false"><main id="c"><p>${_core(id, parent)}</p>${makeIndex(id)}</main><footer id="f"><a href="http://webring.xxiivv.com/#random" target="_blank"><img id="w" src="./img/rotonde.svg"></a><p><a href="./josh.html">Josh Avanier</a> © Éternité</footer></div><script src="./search.js"></script></body></html>`;
   }
 }
