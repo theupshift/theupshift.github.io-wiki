2192e6b2f3ecddcfad38bb0be7eed7ec51a38b23
Josh Avanier <joshavanier@gmail.com>
2019-01-05 13:54:27 +0700

    Declutter

--- archive/js/types/term.js
+++
@@ -1,7 +0,0 @@
-function Term (name, dict = {}, unde = 'Home', type = 'none') {
-  Object.assign(this, {
-    name, type, unde,
-    bref: dict['?'] ? toMarkup(dict['?']) : '',
-    long: new Runic(dict['$']).html()
-  });
-}
