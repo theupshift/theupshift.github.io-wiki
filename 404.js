e3d5617db0958ad8ce99db376efc22849b3b9b6e
Josh Avanier <joshavanier@gmail.com>
2019-01-13 16:42:06 +0700

    Minify

--- 404.js
+++ 404.js
@@ -1,22 +1,2 @@
-function _findSimilar(target, list = lexicon) {
-  let similar = [];
-  for (let i = 0, l = list.length; i < l; i++) {
-    const word = list[i];
-    similar[similar.length] = {
-      word,
-      value: _similarity(target, word)
-    }
-  }
-  return similar.sort((a, b) => a.value - b.value).reverse();
-}
-
-function _similarity(a, b) {
-  let v = 0;
-  for (let i = 0; i < a.length; i++) v += +(b.indexOf(a.substr(i)) > -1);
-  for (let i = 0; i < b.length; i++) v += +(a.indexOf(b.substr(i)) > -1);
-  const c = a.split('').sort().join('');
-  const d = b.split('').sort().join('');
-  for (let i = 0; i < c.length; i++) v += +(d.indexOf(c.substr(i)) > -1);
-  for (let i = 0; i < d.length; i++) v += +(c.indexOf(d.substr(i)) > -1);
-  return v;
-}
+function _findSimilar(c,b){b=void 0===b?lexicon:b;for(var d=[],a=0,f=b.length;a<f;a++){var e=b[a];d[d.length]={word:e,value:_similarity(c,e)}}return d.sort(function(a,b){return a.value-b.value}).reverse()}
+function _similarity(c,b){for(var d=0,a=0;a<c.length;a++)d+=+(-1<b.indexOf(c.substr(a)));for(a=0;a<b.length;a++)d+=+(-1<c.indexOf(b.substr(a)));c=c.split("").sort().join("");b=b.split("").sort().join("");for(a=0;a<c.length;a++)d+=+(-1<b.indexOf(c.substr(a)));for(a=0;a<b.length;a++)d+=+(-1<c.indexOf(b.substr(a)));return d};
