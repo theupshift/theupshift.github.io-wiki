f86f6ac6f22e916c8e5ba873b8709058ae2013ac
Josh Avanier <joshavanier@gmail.com>
2019-03-24 10:33:58 +0700

    Simplify Home

--- build/template/home.js
+++ build/template/home.js
@@ -20,7 +20,6 @@ module.exports = function ({term, type, line}, tables) {
       const term = db[id]
       if (!term.root || n !== term.root) continue
       if (term.term === 'COPYRIGHT') continue;
-      if (term.term === 'OEUVRE') continue;
       temp[temp.length] = term
     }
 
@@ -34,32 +33,13 @@ module.exports = function ({term, type, line}, tables) {
     return scion
   }
 
-  function _getPages () {
-    let pages = [], temp = []
-    for (let id in tables) {
-      const {type} = tables[id];
-      if (type !== 'page') continue;
-      // if (type !== 'note') continue;
-      temp[temp.length] = tables[id];
-    }
-
-    const sorted = Object.keys(temp).sort((a, b) =>
-      (temp[a].term > temp[b].term) ? 1 : -1
-    )
-
-    for (let i = 0, l = sorted.length; i < l; i++)
-      pages[pages.length] = temp[sorted[i]]
-
-    return pages
-  }
-
   /**
    * Insert item into Home Index
    * @param {string} t - Term
    * @return {string} Item
    */
   function _row (t) {
-    return `<a href="${t.toUrl()}.html">${t.toCap()}</a><br>`
+    return `<li><a href="${t.toUrl()}.html">${t.toCap()}</a>`
   }
 
   /**
@@ -67,7 +47,7 @@ module.exports = function ({term, type, line}, tables) {
    * @param {string} name - Term
    * @return {string} Index
    */
-  function _index (name) {
+  function _index (name = term) {
     const scion = _getChildren(name)
     const l = scion.length
     let html = ''
@@ -77,21 +57,9 @@ module.exports = function ({term, type, line}, tables) {
       term !== name && (html += _row(term))
     }
 
-    html += '<a href="https://webring.xxiivv.com/#random">Webring</a>'
+    html += '<li><a href="https://webring.xxiivv.com/#random">Webring</a>'
 
-    return l > 0 ? `<p class="x">${html}` : ''
-  }
-
-  function _glossary () {
-    const pages = _getPages()
-    const l = pages.length
-    let html = ''
-
-    for (let i = 0; i < l; i++) {
-      html += _row(pages[i].term)
-    }
-
-    return l > 0 ? `<p class="x">${html}` : ''
+    return l > 0 ? `<ul>${html}</ul>` : ''
   }
 
   /**
@@ -100,9 +68,7 @@ module.exports = function ({term, type, line}, tables) {
    */
   this.render = () => {
     return [
-      this.head(),
-      `<main>${this.core()}${_index(this.id)}${_glossary()}</main>`,
-      this.footer()
+      this.head(), this.core(), _index(), this.footer()
     ].join('')
   }
 }
