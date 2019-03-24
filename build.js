6f632d9b9a4685bf66a1f59a349b598ea9a2b2be
Josh Avanier <joshavanier@gmail.com>
2019-03-24 11:25:05 +0700

    Remove Lexicon

--- build.js
+++ build.js
@@ -2,7 +2,6 @@ const Indental = require('./build/lib/indental')
 const Builder = require('./build/builder')
 const Manager = require('./build/manager')
 const Database = require('./build/db')
-const Lexicon = require('./build/lexicon')
 const Log = require('./build/log')
 
 const home = require('os').homedir()
@@ -23,4 +22,3 @@ for (let key in db.store) data += db.store[key]
 const {pages} = new Manager(new Indental(data).parse(), logs)
 
 new Builder(pages).build()
-new Lexicon(pages).build()
--- build/lexicon.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-module.exports = function (pages) {
-  this.build = _ => {
-    let info = 'var lexicon = "', tmp = ''
-    for (let i in pages) tmp += `${i}|`
-    info += `${tmp.substring(tmp.length - 1, 0)}".split('|')`
-    fs.writeFile('./l.js', info, (err) => err && console.error(err))
-  }
-}
